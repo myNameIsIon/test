10c4686ba8eb93a7d9a3eca7d77fcb5e6d09b2d1
Ionathan Sideras <iona8ansideras@gmail.com>
2023-10-13 23:51:05 +0300

    commit test

---
+++ app.js
@@ -0,0 +1,7 @@
+const div = document.createElement('div');
+const body = document.querySelector('body');
+
+div.textContent = 'hii'
+div.style.cssText = 'background-Color: black; color: white;'
+
+body.appendChild(div);
